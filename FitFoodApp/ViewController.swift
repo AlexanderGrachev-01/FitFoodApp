fb49cdb68eed3a2f85d1fb7b7d4262405cd3350b
Alexander <62618072+AlexanderGrachev-01@users.noreply.github.com>
2021-12-21 00:24:24 +0300

    Initial Commit

---
+++ FitFoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitFoodApp
+//
+//  Created by Александр Грачев on 21.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
