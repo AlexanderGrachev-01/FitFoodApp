4aa76f3fb6a80559a78924b5ee81d3300354c16a
Alexander <62618072+AlexanderGrachev-01@users.noreply.github.com>
2021-12-22 18:46:56 +0300

    MainPage connected to code

---
+++ FitFoodApp/MainPageVC.swift
@@ -0,0 +1,31 @@
+//
+//  MainPageVC.swift
+//  FitFoodApp
+//
+//  Created by Александр Грачев on 22.12.2021.
+//
+
+import UIKit
+
+class MainPageVC: UIViewController {
+
+    @IBOutlet weak var kcalsWindowView: UIView!
+    @IBOutlet weak var kcalCounterLabel: UILabel!
+    @IBOutlet weak var carbsWindowView: UIView!
+    @IBOutlet weak var carbsCounterLabel: UILabel!
+    @IBOutlet weak var proteinWindowView: UIView!
+    @IBOutlet weak var proteinCounterLabel: UILabel!
+    @IBOutlet weak var fatWindowView: UIView!
+    @IBOutlet weak var fatCounterLabel: UILabel!
+    @IBOutlet weak var addBreakfastView: UIView!
+    @IBOutlet weak var addLunchView: UIView!
+    @IBOutlet weak var addDinerView: UIView!
+    @IBOutlet weak var addSnacksView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
