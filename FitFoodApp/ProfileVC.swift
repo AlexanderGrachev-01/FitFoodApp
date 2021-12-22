a082afa54c436827093abe8b29452186ad3d7f84
Alexander <62618072+AlexanderGrachev-01@users.noreply.github.com>
2021-12-22 21:14:55 +0300

    ProfileVC added

---
+++ FitFoodApp/ProfileVC.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileVC.swift
+//  FitFoodApp
+//
+//  Created by Александр Грачев on 22.12.2021.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
